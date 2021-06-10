6537d83694d73e23e70cc1a90d71a8fdd603c168
Yannick Francois <yaf@elsif.fr>
2021-06-10 18:46:49 +0200

    détail un test et ajoute nombre négatif

--- fibonacci/spec/FibonacciSpec.js
+++ fibonacci/spec/FibonacciSpec.js
@@ -1,14 +1,26 @@
 describe("Fibonacci", function() {
 
   it("donne10PremiersElementsFibo renvoie un tableau", function() {
-    expect(donne10PremiersElementsFibo(0)).toBeInstanceOf(Array);
+    expect(donne10PremiersElementsFibo(54)).toBeInstanceOf(Array);
   });
 
   it("donne10PremiersElementsFibo(0) renvoie un tableau vide", function() {
     expect(donne10PremiersElementsFibo(0)).toEqual([]);
   });
 
+  it("renvoie un tableau vide en cas de nombre négatif", function() {
+    expect(donne10PremiersElementsFibo(-2)).toEqual([]);
+  });
+
   it("donne10PremiersElementsFibo(1) renvoie [0]", function() {
-    expect(donne10PremiersElementsFibo(1)).toEqual([0]);
+    // Définir ce dont on a besoin
+    let resultatAttendu = [0]
+    let nombreDElementDemande = 1
+
+    // Executé l'action
+    let resultat = donne10PremiersElementsFibo(nombreDElementDemande)
+
+    // Vérifier le résultat
+    expect(resultat).toEqual(resultatAttendu);
   });
 });
