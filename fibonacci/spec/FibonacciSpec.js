7913450fe1a75ad305df35e450f3ac75cc841095
Yannick Francois <yaf@elsif.fr>
2021-06-10 18:38:59 +0200

    renvoie un tableau vide pour fibo(0)

--- fibonacci/spec/FibonacciSpec.js
+++ fibonacci/spec/FibonacciSpec.js
@@ -1,7 +1,10 @@
 describe("Fibonacci", function() {
 
   it("donne10PremiersElementsFibo renvoie un tableau", function() {
-    expect(donne10PremiersElementsFibo()).toBeInstanceOf(Array);
+    expect(donne10PremiersElementsFibo(0)).toBeInstanceOf(Array);
   });
 
+  it("donne10PremiersElementsFibo(0) renvoie un tableau vide", function() {
+    expect(donne10PremiersElementsFibo(0)).toEqual([]);
+  });
 });
--- fibonacci/src/Fibonacci.js
+++ fibonacci/src/Fibonacci.js
@@ -1,4 +1,4 @@
 
 function donne10PremiersElementsFibo() {
-    return [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    return []
 }
