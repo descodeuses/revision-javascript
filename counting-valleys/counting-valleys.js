d7ddb6db10a837b7bdb089988f658e0d5ffa584c
Yannick Francois <yaf@elsif.fr>
2021-06-22 18:25:40 +0200

    utilise une autre structure de contrôle

--- counting-valleys/counting-valleys.js
+++ counting-valleys/counting-valleys.js
@@ -22,21 +22,28 @@ Structure de contrôle permettant de détecter une valée
     Si pas qui m'a fait revenir à 0 est un U, alors j'étais dans une vallée
 
 */
+const jeViensDUneVallee = (positionAgainstSeaLevel, step) => {
+    return positionAgainstSeaLevel == 0 && step == "U"
+}
+
 const countNumberOfValleysFor = (steps) => {
 
     let positionAgainstSeaLevel = 0
     let numberOfValleys = 0
     for(step of steps) {
-        if (step == "U") {
-            positionAgainstSeaLevel++
-        } else if (step == "D") {
-            positionAgainstSeaLevel--
+        switch (step) {
+            case 'U':
+                positionAgainstSeaLevel++
+                break
+            case 'D':
+                positionAgainstSeaLevel--
+                break
         }
         // DEBUG.
         // console.log(step, positionAgainstSeaLevel)
-        if (positionAgainstSeaLevel == 0 && step == "U") {
+        if (jeViensDUneVallee(positionAgainstSeaLevel, step)) {
             numberOfValleys++
-        }
+        }        
     }
 
     return numberOfValleys
