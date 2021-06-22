a57e3ac54c811e5c5ac51bb236085553f040c856
Yannick Francois <yaf@elsif.fr>
2021-06-22 18:00:18 +0200

    Décrit l'algoritme

--- counting-valleys/counting-valleys.js
+++ counting-valleys/counting-valleys.js
@@ -1,2 +1,33 @@
 console.log("fichier counting-valleys.js chargé !")
 
+/*
+UDDDUDUU => 1
+
+Algoritme pour compter le nombre de vallées
+
+Boucle de consommation des pas :
+    Je prend le premier pas.
+    Si c'est un U, je compte +1
+    Sinon si c'est un D, je compte -1
+
+    Je prend le second pas.
+    Si c'est un U, je compte +1
+    Sinon si c'est un D, je compte -1
+
+    Et ainsi de suite
+
+Structure de contrôle permettant de détecter une valée
+
+    Quand je reviens à 0, j'ai fini une montagne, ou une vallée.
+    Si pas qui m'a fait revenir à 0 est un U, alors j'étais dans une vallée
+
+*/
+const countNumberOfValleysFor = (steps) => {
+    let splittedSteps = steps.split("")
+    let numberOfU = splittedSteps.filter((e) => { return e == "U"}).length
+    let numberOfD = splittedSteps.filter((e) => { return e == "D"}).length
+    console.log(splittedSteps, numberOfU,numberOfD )
+    console.log(numberOfU == numberOfD)
+
+    return 0
+}
--- counting-valleys/glue.js
+++ counting-valleys/glue.js
@@ -3,8 +3,6 @@ console.log("fichier glue.js chargé !")
 document.getElementById("count").addEventListener("click", function (){
     let steps = document.getElementById("steps").value
     console.log("steps : ", steps)
-    //let numberOfValleys = countNumberOfValleysFor(steps)
-    let numberOfValleys = 0
-    
+    let numberOfValleys = countNumberOfValleysFor(steps)
     document.getElementById("resultat").innerHTML = numberOfValleys;
 })
